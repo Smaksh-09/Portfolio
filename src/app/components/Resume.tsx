eec70cbfc763232b7c4a8d7aace49c8545c59f65
Smaksh-09 <smakshdhawan@gmail.com>
2025-01-31 18:58:54 +0530

    addes the resume link

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 export default function Resume() {
+  const handleResumeClick = () => {
+    window.open("https://drive.google.com/file/d/1U6lfIlmIvi_yVMiNxchRgEQ91-Sg9b4O/view?usp=drive_link", "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="relative px-4 sm:px-6 md:absolute md:top-[350px] md:left-24">
       <button
-        onClick={() => {}}
+        onClick={handleResumeClick}
         className="relative px-6 py-3 rounded-full font-medium text-white 
           bg-gradient-to-r from-purple-400 to-pink-400
           hover:from-purple-500 hover:to-pink-500
